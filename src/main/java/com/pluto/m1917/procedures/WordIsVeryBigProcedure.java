5c00620b57f6487abe0e3ba88918b305b676eb15
Plutomw <86793942+Plutomw@users.noreply.github.com>
2021-08-17 11:16:59 +0800

    提高牙签威慑力

--- src/main/java/com/pluto/m1917/procedures/WordIsVeryBigProcedure.java
+++ src/main/java/com/pluto/m1917/procedures/WordIsVeryBigProcedure.java
@@ -20,14 +20,8 @@ public class WordIsVeryBigProcedure extends M1917gunModElements.ModElement {
 	}
 
 	public static void executeProcedure(Map<String, Object> dependencies) {
-		if (dependencies.get("x") == null) {
-		}
-		if (dependencies.get("y") == null) {
-		}
-		if (dependencies.get("z") == null) {
-		}
-		if (dependencies.get("world") == null) {
-		}
+		Entity entity = (Entity) dependencies.get("entity");
+		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
 		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
@@ -42,6 +36,14 @@ public class WordIsVeryBigProcedure extends M1917gunModElements.ModElement {
 					SoundCategory.NEUTRAL, (float) 1, (float) 1, false);
 		}
 		if (entity instanceof PlayerEntity)
-			((PlayerEntity) entity).getCooldownTracker().setCooldown(((itemstack)).getItem(), (int) 20);
+			((PlayerEntity) entity).getCooldownTracker().setCooldown(((itemstack)).getItem(), (int) 600);
+		((itemstack)).shrink((int) 1);
+		{
+			Entity _ent = entity;
+			if (!_ent.world.isRemote && _ent.world.getServer() != null) {
+				_ent.world.getServer().getCommandManager().handleCommand(_ent.getCommandSource().withFeedbackDisabled().withPermissionLevel(4),
+						"tell @a \u00A7c\u00A7l\u6211\u7684\u5F88\u5927\uFF0C\u8BF7\u5FCD\u4E00\u4E0B ");
+			}
+		}
 	}
 }
