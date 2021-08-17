63c665fb48d4fbcff68fa34e670290f304a3e402
Plutomw <86793942+Plutomw@users.noreply.github.com>
2021-08-17 11:23:16 +0800

    把音频直接删了

--- src/main/java/com/pluto/m1917/procedures/WordIsVeryBigProcedure.java
+++ src/main/java/com/pluto/m1917/procedures/WordIsVeryBigProcedure.java
@@ -22,19 +22,6 @@ public class WordIsVeryBigProcedure extends M1917gunModElements.ModElement {
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		Entity entity = (Entity) dependencies.get("entity");
 		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
-		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
-		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
-		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
-		IWorld world = (IWorld) dependencies.get("world");
-		if (world instanceof World && !world.isRemote()) {
-			((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
-					(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("m1917gun:wordsbig")),
-					SoundCategory.NEUTRAL, (float) 1, (float) 1);
-		} else {
-			((World) world).playSound(x, y, z,
-					(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("m1917gun:wordsbig")),
-					SoundCategory.NEUTRAL, (float) 1, (float) 1, false);
-		}
 		if (entity instanceof PlayerEntity)
 			((PlayerEntity) entity).getCooldownTracker().setCooldown(((itemstack)).getItem(), (int) 600);
 		((itemstack)).shrink((int) 1);
